07ec4e2e614ec508b89b157ec558617e253c8129
Shikuci <sw23d020@mandakh.edu.mn>
2025-09-09 16:28:59 +0800

    lab2

---
+++ app/about/page.tsx
@@ -0,0 +1,20 @@
+import styles from "./page.module.css";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import Layout from "@/components/Layout";
+
+export default function About() {
+  return (
+    <Layout>
+      <div className="p-8">
+        <h1 className="text-3xl font-bold text-blue-600">Миний тухай</h1>
+        <p className="mt-4 text-gray-700">
+          Би Next.js ашиглаж веб хөгжүүлэлт сурч байна.
+        </p>
+        <p className="mt-2">Миний хобби: унших, аялах, программчлал.</p>
+      </div>
+    </Layout>
+  );
+}
+
+
